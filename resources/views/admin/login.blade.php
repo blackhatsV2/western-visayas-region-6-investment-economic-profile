cdae95fc1a5118ab316ce803920419f167b4b0f3
blackhatsV1 <brimelasting@gmail.com>
2026-02-19 13:09:57 +0800

    feat: Add Alpine.js to the admin login page.

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -7,6 +7,7 @@
     <link rel="icon" type="image/png" href="{{ asset('dti-logo.png') }}">
     
     <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     
     <script>
